f78baf125a1ef8a0973c3bd0062d6d7184c09d25
Adrian Garcia Rojas <adrian_gar016@hotmail.es>
2019-06-26 22:53:57 -0500

    generando la vista de las listas de tareas, ahora en estado empty

---
+++ app/src/main/java/com/adriangarciarojas/listadetareas/PanelDeTareas/View/ViewPanelDeTarelas.java
@@ -0,0 +1,16 @@
+package com.adriangarciarojas.listadetareas.PanelDeTareas.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.adriangarciarojas.listadetareas.R;
+
+public class ViewPanelDeTarelas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_panel_de_tarelas);
+    }
+}
