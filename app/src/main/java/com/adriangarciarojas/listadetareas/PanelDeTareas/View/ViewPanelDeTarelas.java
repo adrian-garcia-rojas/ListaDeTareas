2803ff8fe2a08b1ee18a19def825aa92cacb8a99
Adrian Garcia Rojas <adrian_gar016@hotmail.es>
2020-02-16 10:15:08 -0600

    punto de control

--- app/src/main/java/com/adriangarciarojas/listadetareas/PanelDeTareas/View/ViewPanelDeTarelas.java
+++ app/src/main/java/com/adriangarciarojas/listadetareas/PanelDeTareas/View/ViewPanelDeTarelas.java
@@ -3,6 +3,7 @@ package com.adriangarciarojas.listadetareas.PanelDeTareas.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.adriangarciarojas.listadetareas.R;
 
@@ -12,5 +13,13 @@ public class ViewPanelDeTarelas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_panel_de_tarelas);
+        
+        
+        
+        
+    }
+    
+    public void metodosDePrueba(){
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
 }
